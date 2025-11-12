0c486a00b738e1b20cb2f9694523b993e62dccf1
Jahnabi Sharma <jahnabi.s@terrapay.com>
2025-11-12 06:37:20 +0530

    updated

--- README.md
+++
@@ -1,1 +0,0 @@
-# order-inventory-microservices-assignment
---
+++ order/src/main/java/order/OrderServiceApp.java
@@ -0,0 +1,15 @@
+package order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class OrderServiceApp {
+
+	public static void main(String[] args) {	
+		SpringApplication.run(OrderServiceApp.class, args);
+
+	}
+
+}
